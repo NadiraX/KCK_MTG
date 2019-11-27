b4797fa46fcf3e54ff4d47c2ef0046bd449e558d
NadiraX <57359646+NadiraX@users.noreply.github.com>
2019-11-27 22:07:28 +0100

    Update pure_reader.py

--- pure_reader.py
+++ pure_reader.py
@@ -10,8 +10,10 @@ def gora_dol(imgs,pure_list):
         hsv_im = imgs[i].convert('HSV')
         width, height = imgs[i].size
 
-        h2 = height / 3
-        print(h2)
+        if width > height:
+            imgs[i] = imgs[i].rotate(90)
+
+        h2 = height / 8
         gora = []
         dol = []
 
@@ -24,20 +26,20 @@ def gora_dol(imgs,pure_list):
                 r, g, b = hsv_im.getpixel((w, h))
                 v, s, h3 = colorsys.rgb_to_hsv(r, g, b)
 
-                if h > h2*2:
+                if h > h2*5 and h<h2*7:
                     suma1 += v
                     iter1 += 1
-                elif h<h2:
+                elif h<h2*3 and h>h2:
                     suma2 += v
                     iter2 += 1
         print(pure_list[i])
         if suma1/iter1 >suma2/iter2:
-            #print("1")
+            print("1")
             pass
         else:
             pass
-            #print("0")
-
+            print("0")
+        imgs[i].show()
 def main():
     pure_list = []
     save_path_pure = Path('pure/')
@@ -45,10 +47,10 @@ def main():
         pure_list.append(filename)
 
     imgs = [Image.open(os.path.join(save_path_pure, Path(i))) for i in pure_list]
-#   for i in range(len(imgs)):
-#        imgs[i]=imgs[i].filter(ImageFilter.SHARPEN)
-
-
+    # for i in range(len(imgs)):
+    #     imgs[i] = imgs[i].convert('L')
+    #     imgs[i] = imgs[i].point(lambda x: 0 if x < 128 else 255, '1')
+    #     #imgs[i].show()
     gora_dol(imgs,pure_list)
 
 main()
