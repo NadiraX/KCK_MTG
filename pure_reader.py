67e70ca524f28caad7f0b08b9aa5a7c9aa403b48
NadiraX <57359646+NadiraX@users.noreply.github.com>
2019-11-28 11:03:07 +0100

    adjustment

--- pure_reader.py
+++ pure_reader.py
@@ -1,18 +1,18 @@
 import os.path
-import cv2 as cv
 from pathlib import Path
 from PIL import Image
-from PIL import ImageFilter
 import colorsys
 
 def gora_dol(imgs,pure_list):
+    rotator=[0 for i in imgs]
     for i in range(len(imgs)):
-
+        imgs[i] = imgs[i].convert('L')
+        imgs[i] = imgs[i].point(lambda x: 0 if x < 128 else 255, '1')
         hsv_im = imgs[i].convert('HSV')
-        #imgs[i].show()
         width, height = imgs[i].size
-
-
+        if width > height:
+            imgs[i] = imgs[i].rotate(90,expand=True)
+            rotator[i] += 90
 
         h2 = height / 2
 
@@ -32,14 +32,13 @@ def gora_dol(imgs,pure_list):
                 elif h<h2:
                     suma2 += v
                     iter2 += 1
-        print(pure_list[i])
         if (suma1/iter1) >(suma2/iter2):
-            print("1")
+            pass
         else:
-            print("0")
-            imgs[i] = imgs[i].rotate(180, expand=True)
-        #imgs[i].show()
-    return(imgs)
+            rotator[i]+=180
+    return(rotator)
+
+
 def main():
     pure_list = []
     save_path_pure = Path('pure/')
@@ -47,13 +46,10 @@ def main():
         pure_list.append(filename)
 
     imgs = [Image.open(os.path.join(save_path_pure, Path(i))) for i in pure_list]
+
+    rotator=gora_dol(imgs.copy(),pure_list)
+
     for i in range(len(imgs)):
-        width, height = imgs[i].size
-        if width > height:
-            imgs[i] = imgs[i].rotate(90,expand=True)
-        imgs[i] = imgs[i].convert('L')
-        imgs[i] = imgs[i].point(lambda x: 0 if x < 128 else 255, '1')
-    imgs=gora_dol(imgs,pure_list)
-    for i in range(len(imgs)):
-        imgs[i].save(pure_list[i])
+        imgs[i] = imgs[i].rotate(rotator[i], expand=True)
+        imgs[i].save(os.path.join(save_path_pure,pure_list[i]))
 main()
